dccd1daddb0e54fadd5b06a8ff26768f861b74d9
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-02-04 12:13:06 +0100

    logó és szélesség

--- frontend/src/components/app/AppNav.vue
+++ frontend/src/components/app/AppNav.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+</script>
+
 <template>
     <nav class="navbar navbar-expand-lg bg-black">
-        <div class="container-fluid">
+        <div class="container-lg">
+            <RouterLink :to="{ name: 'home' }" class="navbar-brand" href="#">
+                <img src="/logo.png" width="40" height="40" class="img-fluid" />
+            </RouterLink>
             <button class="navbar-toggler border-white ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="bi bi-list text-white p-2 fs-3"></i>
             </button>
