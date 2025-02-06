7a3548dfc57b1adf8445c2c2af6ec6351a626413
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-02-06 13:25:42 +0100

    főoldal szépítgetések

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HeroImage from '@/assets/img/hero.png';
+import DeliveryAdImage from '@/assets/img/delivery_ad.jpg';
 import StoreSection from '@/components/store/StoreSection.vue';
 import { useMeals } from '@/composables/useMeals';
 
@@ -8,20 +9,45 @@ const meals = useMeals();
 
 <template>
     <main class="container-lg">
-        <section class="row flex-lg-row-reverse align-items-center g-5 py-4 px-5 bg-black bg-gradient text-white rounded-5 mt-md-4 mb-2 shadow-lg">
-            <div class="col-10 col-sm-8 col-lg-6 mx-auto d-none d-lg-flex">
+        <section class="row flex-lg-row-reverse align-items-center g-5 py-5 px-5 bg-black bg-gradient text-white mt-lg-4 mb-2 ad">
+            <div class="col-10 col-sm-8 col-lg-6 mx-auto d-none d-lg-flex my-auto">
                 <img :src="HeroImage" class="d-block mx-lg-auto img-fluid" alt="" width="500" height="500">
             </div>
             <div class="col-lg-6 text-lg-start text-center">
                 <h1 class="display-4 fw-bold lh-1 mb-3 text-glow">Rendelj kényelmesen</h1>
-                <p class="lead">Éhes vagy? Megoldjuk! Csupán annyit kell tenned, hogy megrendeled a kedvenc ételed az
-                    alábbi katalógusból.</p>
-                <p class="lead text-white-50 fs-6">Kép: pngtree.com</p>
+                <p class="lead mb-0 mb-lg-3">Éhes vagy? Megoldjuk! Csupán annyit kell tenned, hogy megrendeled a kedvenc ételed az alábbi katalógusból.</p>
+                <p class="lead text-white-50 fs-6 d-lg-inline-block d-none">Kép: pngtree.com</p>
             </div>
         </section>
         <StoreSection :meals="meals.MENU" title="Menük" v-if="meals.MENU.length > 0" />
         <StoreSection :meals="meals.FOOD" title="Ételek" v-if="meals.FOOD.length > 0" />
+        <section class="row flex-lg-row-reverse align-items-center justify-content-center g-5 bg-black bg-gradient text-white mt-lg-4 my-2 overflow-hidden ad">
+            <div class="col-lg-6 text-lg-start text-center my-auto py-5 px-5">
+                <h1 class="display-4 fw-bold lh-1 mb-3 text-glow">Gyors & precíz kiszállítás</h1>
+                <p class="lead mb-0 mb-lg-3">Budapesten belül 45, Pest megyén belül pedig 90 perc alatt az asztalodon lehet a rendelésed.</p>
+                <p class="lead text-white-50 fs-6 d-lg-inline-block d-none mb-0">Kép: pexels.com</p>
+                <RouterLink :to="{ name: 'register' }" class="btn btn-light d-block rounded-pill mt-4 fw-bold btn-cta px-4 py-2 mx-auto mx-lg-0">REGISZTRÁLJ</RouterLink>
+            </div>
+            <img :src="DeliveryAdImage" class="object-fit-cover col-10 col-sm-8 col-lg-6 d-none d-lg-block p-0 m-0 img-fluid" alt="">
+        </section>
         <StoreSection :meals="meals.BEVERAGE" title="Italok" v-if="meals.BEVERAGE.length > 0" />
         <StoreSection :meals="meals.DESSERT" title="Desszertek" v-if="meals.DESSERT.length > 0" />
     </main>
 </template>
+
+<style>
+  .ad {
+    border-radius: 2rem;
+  }
+
+  .btn-cta {
+    width: fit-content;
+    font-size: 1.1rem;
+  }
+
+  @media (max-width: 992px) {
+    .ad {
+      border-radius: 0;
+    }
+  }
+</style>
