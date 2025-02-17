f1dc87f70c508f26c77d5b8ccd51c40822898361
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-02-17 15:11:38 +0100

    rendelő felület szépítés, jobbá tétel

--- frontend/src/components/store/CartSummary.vue
+++ frontend/src/components/store/CartSummary.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { useUser } from '@/composables/useUser';
-import { computed, ref } from 'vue';
+import { computed, ref, watch, type InputHTMLAttributes } from 'vue';
+import ImageKHSzepkartya from '@/assets/payment/kh.png';
+import ImageOTPSzepkartya from '@/assets/payment/otp.png';
+import ImageMBHSzepkartya from '@/assets/payment/mbh.png';
+import ImageCashAccepted from '@/assets/payment/cash.png';
+
+const paymentProcessors = [ImageKHSzepkartya, ImageOTPSzepkartya, ImageMBHSzepkartya, ImageCashAccepted];
 
 defineProps<{
     totalCost: number;
@@ -11,35 +17,98 @@ defineProps<{
 const user = await useUser();
 const promptForLogin = computed(() => !!user);
 
-const requestShipping = ref(false);
-const deliveryAddress = ref("");
+interface BaseDeliveryMethod {
+  label: string;
+  id: string;
+  requiresCustomInput: boolean;
+}
+
+interface DeliveryMethodWithCustomInput extends BaseDeliveryMethod {
+  requiresCustomInput: true;
+  customInputLabel: string;
+  customInputDescription: string;
+  customInputPlaceholder: string;
+  customInputType: InputHTMLAttributes["type"];
+}
+
+interface DeliveryMethodWithoutCustomInput extends BaseDeliveryMethod {
+  requiresCustomInput: false;
+  customInputLabel?: undefined;
+  customInputDescription?: undefined;
+  customInputPlaceholder?: undefined;
+  customInputType?: undefined;
+}
+
+type DeliveryMethod = DeliveryMethodWithCustomInput | DeliveryMethodWithoutCustomInput;
+
+const deliveryMethods: DeliveryMethod[] = [
+    {
+        label: "Átvétel a pultnál",
+        id: "checkout",
+        requiresCustomInput: false,
+    },
+    {
+        label: "Átvétel az asztalnál",
+        id: "table",
+        requiresCustomInput: true,
+        customInputLabel: "Asztalszám",
+        customInputDescription: "Add meg annak az asztalnak a számát, ahol ülsz. Az asztalszám az asztal közepén található.",
+        customInputPlaceholder: "Asztalszám",
+        customInputType: "number",
+    },
+    {
+        label: "Kiszállítás futárral",
+        id: "delivery",
+        requiresCustomInput: true,
+        customInputLabel: "Kiszállítási cím",
+        customInputDescription: "Budapesten belül 45, Pest megyén belül pedig 90 perc alatt garantáljuk a kiszállítást. Kérjük, hogy ne adj meg más címet, mert a rendelést törölni fogjuk.",
+        customInputPlaceholder: "1234 Budapest, Példa u. 1",
+        customInputType: "text",
+    }
+];
+
+const customInputValue = ref<string | undefined>(undefined);
+const chosenDeliveryMethod = ref<DeliveryMethod>(deliveryMethods[0]);
+
+watch(chosenDeliveryMethod, () => {
+    customInputValue.value = "";
+});
 </script>
 
 <template>
-    <form class="row g-5 my-5">
-        <h1 class="display-4 fw-bold lh-1 mb-3 text-glow">Összesítés</h1>
+    <form class="row my-5">
+        <h1 class="display-4 fw-bold lh-1 mb-md-3 mb-2  m-0 text-glow">Összesítés</h1>
         <p class="m-0">Már csak pár lépés és nemsokára az asztalodon landol a rendelésed. Kérjük, hogy valós adatokat adj meg.</p>
-        <hr class="my-4">
-        <div class="col-9">
+        <hr class="my-4 my-md-5">
+        <div class="col-sm-auto col-md-9">
             <div class="mb-4">
                 <label for="inputDeliveryType" class="form-label text-uppercase fw-bold mb-2">Átvétel formája</label>
-                <select id="inputDeliveryType" class="form-select border-dark rounded-3" aria-describedby="deliveryTypeHelp">
-                    <option v-for="location in locations" :key="location">{{ location }}</option>
+                <select id="inputDeliveryType" class="form-select border-dark rounded-3" aria-describedby="deliveryTypeHelp" v-model="chosenDeliveryMethod">
+                    <option v-for="method in deliveryMethods" :key="method.id" :value="method">{{ method.label }}</option>
                 </select>
             </div>
-            <div class="mb-3">
-                <label for="inputAddress" class="form-label text-uppercase fw-bold mb-2">Kiszállítási cím</label>
-                <input type="text" class="form-control border-dark rounded-3" id="inputAddress" aria-describedby="addressHelp">
-                <div id="addressHelp" class="form-text">Budapesten belül 45, Pest megyén belül pedig 90 perc alatt garantáljuk a kiszállítást. Kérjük, hogy ne adj meg más címet, mert a rendelést törölni fogjuk.</div>
+            <div class="mb-3" v-if="chosenDeliveryMethod.requiresCustomInput">
+                <label for="inputAddress" class="form-label text-uppercase fw-bold mb-2">{{ chosenDeliveryMethod.customInputLabel }}</label>
+                <input :type="chosenDeliveryMethod.customInputType" class="form-control border-dark rounded-3" id="inputAddress" aria-describedby="addressHelp" v-model="customInputValue" :placeholder="chosenDeliveryMethod.customInputPlaceholder">
+                <div id="addressHelp" class="form-text">{{ chosenDeliveryMethod.customInputDescription }}</div>
             </div>
         </div>
-        <div class="col-3">
+        <div class="col-sm-auto col-md-3">
             <div class="mb-3 card rounded-4">
                 <div class="card-body">
-                    <p class="text-uppercase fw-bold mb-2">Fizetendő</p>
+                    <p class="text-uppercase fw-bold mb-1">Fizetendő</p>
                     <div class="display-5 fw-bold m-0">{{ totalCost }} Ft</div>
-                    <p class="form-text mt-2">A kalkuláció tartalmazza az adókat és szállítási díjat.</p>
-                    <RouterLink :to="{ name: 'register' }" class="btn btn-dark d-block rounded-pill mt-4 fw-bold px-4 py-2">MEGRENDELEM</RouterLink>
+                    <p class="form-text mt-2">A kalkuláció <b>nem tartalmazza</b> az adókat és szállítási díjat. Fizetni a
+                        <b v-if="chosenDeliveryMethod.id === 'checkout'">kasszánál</b>
+                        <b v-else-if="chosenDeliveryMethod.id === 'table'">pincérnél</b>
+                        <b v-else-if="chosenDeliveryMethod.id === 'delivery'">futárnál</b>
+                        tudsz majd.</p>
+                        <hr>
+                        <RouterLink :to="{ name: 'register' }" class="btn btn-dark d-block rounded-pill mt-4 fw-bold px-4 py-2">MEGRENDELEM</RouterLink>
+                    <div class="row gap-2 justify-content-center mt-4">
+                        <img :src="processor" alt="" width="40" height="26" class="col-auto" v-for="processor in paymentProcessors" :key="processor">
+                    </div>
+                    <p class="form-text text-center mt-3 mb-0">Szépkártya, hitelkártya, bankkártya, készpénz elfogadóhely</p>
                 </div>
             </div>
         </div>
--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -18,19 +18,14 @@ const mealsRef = useMeals(ids.value);
 const totalValue = computed(() => meals.value.reduce((a, b) => a + b.price , 0));
 
 watchEffect(() => {
-    meals.value = ids.value.length > 0 ? mealsRef.value : [];
+    meals.value = ids.value.length > 0 ? mealsRef.value : [];   
 });
 </script>
 
 <template>
     <main class="container-lg">
-        <header class="mb-4 pt-3 row">
-            <div class="col">
-                <h2 class="display-4 fw-bold">Kosár ({{ totalValue }} Ft)</h2>
-            </div>
-            <div class="col">
-                <button type="button" class="btn btn-dark fw-bold w-100 mt-auto rounded-pill text-uppercase">MEGRENDELEM</button>
-            </div>
+        <header class="mb-4 pt-3">
+            <h2 class="display-4 fw-bold">Kosár ({{ meals.length }} elem)</h2>
         </header>
         <section class="d-flex flex-column gap-3">
             <CartItem v-for="meal in meals" :key="meal.id" :meal="meal" />
