f939770c709d0212c8bfc32b6e5c99913c57b9b3
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-03-18 08:14:13 +0100

    űrlap validáció ux

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -59,22 +59,40 @@ watch([session], onSessionPreset);
             <form class="card-body p-3" @submit.prevent="submit">
                     <AppAlert :text="error.error" :type="error?.css_class" v-if="error" />
                     <div class="mb-3">
-                        <label for="fullNameHelp" class="form-label text-uppercase fw-bold">Teljes név</label>
-                        <input type="text" class="form-control border-dark rounded-3" id="inputEmail" aria-describedby="fullNameHelp" v-model="fullName" placeholder="Példa Pista">
-                        <div id="fullNameHelp" class="form-text">A rendelések beazonosításához szükséges</div>
+                        <label for="inputFullName" class="form-label text-uppercase fw-bold">Teljes név</label>
+                        <input type="text" class="form-control rounded-3" id="inputFullName" aria-describedby="fullNameHelp" v-model.trim="fullName" placeholder="Példa Pista" :class="{
+                            'border-dark': fullName.trim().length > 0,
+                            'is-invalid': fullName.trim().length === 0,
+                        }">
+                        <div id="fullNameHelp" class="form-text" :class="{
+                            'valid-feedback': fullName.trim().length > 0,
+                            'invalid-feedback': fullName.trim().length === 0,
+                        }">A rendelések beazonosításához szükséges</div>
                     </div>
                     <div class="mb-3">
                         <label for="inputEmail" class="form-label text-uppercase fw-bold">Email cím</label>
-                        <input type="email" class="form-control border-dark rounded-3" id="inputEmail" aria-describedby="emailHelp" v-model="email" placeholder="valaki@pelda.hu">
-                        <div id="emailHelp" class="form-text">Kérjük, adj meg egy érvényes címet</div>
+                        <input type="email" class="form-control rounded-3" id="inputEmail" aria-describedby="emailHelp" v-model.trim="email" placeholder="valaki@pelda.hu" :class="{
+                            'border-dark': email.trim().length > 0,
+                            'is-invalid': email.trim().length === 0,
+                        }">
+                        <div id="emailHelp" class="form-text" :class="{
+                            'valid-feedback': email.trim().length > 0,
+                            'invalid-feedback': email.trim().length === 0,
+                        }">Kérjük, adj meg egy érvényes címet</div>
                     </div>
                     <div class="mb-3">
                         <label for="inputPassword" class="form-label text-uppercase fw-bold">Jelszó</label>
-                        <input type="password" class="form-control border-dark rounded-3" id="inputPassword" aria-describedby="passwordHelp" v-model="password" placeholder="Jelszó">
-                        <div id="passwordHelp" class="form-text">Legalább 6 karakter hosszú kell legyen</div>
+                        <input type="password" class="form-control rounded-3" id="inputPassword" aria-describedby="passwordHelp" v-model.trim="password" placeholder="Jelszó" :class="{
+                            'border-dark': password.trim().length > 0,
+                            'is-invalid': password.trim().length === 0,
+                        }">
+                        <div id="passwordHelp" class="form-text" :class="{
+                            'valid-feedback': password.trim().length > 0,
+                            'invalid-feedback': password.trim().length === 0,
+                        }">Legalább 6 karakter hosszú kell legyen</div>
                     </div>
                     <div class="mb-3">
-                        <button type="submit" class="btn btn-dark fw-bold w-100 rounded-pill text-uppercase">Regisztrálok</button>
+                        <button type="submit" class="btn btn-dark fw-bold w-100 rounded-pill text-uppercase" :disabled="email.length === 0 || password.length === 0 || fullName.length == 0">Regisztrálok</button>
                     </div>
                     <RouterLink class="btn btn-outline-dark fw-bold w-100 rounded-pill text-uppercase" :to="{ name: 'login' }">Bejelentkezek</RouterLink>
                 </form>
