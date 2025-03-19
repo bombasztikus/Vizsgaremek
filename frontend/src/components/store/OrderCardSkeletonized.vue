28540716217fc892f5070341f6d918df5926f0d3
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-03-19 14:15:34 +0100

    skeleton loader a rendelésekhez

---
+++ frontend/src/components/store/OrderCardSkeletonized.vue
@@ -0,0 +1,13 @@
+<template>
+    <article class="card rounded-4">
+        <div class="card-body">
+            <p class="card-title fs-4 mb-1 lh-1 placeholder-glow">
+                <span class="placeholder col-3"></span>
+            </p>
+            <div class="card-text placeholder-glow">
+                <span class="placeholder me-2" style="width: 1rem;"></span>
+                <span class="placeholder col-2"></span>
+            </div>
+        </div>
+    </article>
+</template>
