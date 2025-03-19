86e592b43bd0386f2c7ad2acfcb4ff6ceab4ac81
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-03-19 22:41:33 +0100

    hover animáció

--- frontend/src/components/store/ContinueCard.vue
+++ frontend/src/components/store/ContinueCard.vue
@@ -10,7 +10,7 @@ defineProps<{
     <RouterLink :to="to" class="d-block text-decoration-none h-100">
         <div class="card h-100 rounded-4 overflow-hidden text-body-secondary" style="min-height: 400;">
             <div class="card-body d-flex flex-column gap-4 justify-content-center align-items-center">
-                <i class="bi bi-arrow-right-circle-fill display-1"></i>
+                <i class="bi bi-arrow-right-circle-fill display-1" :class="[$style.grow]"></i>
                 <h5 class="card-title fs-4 lh-base m-0 fw-semibold">Továbbiak</h5>
             </div>
         </div>
@@ -22,7 +22,7 @@ defineProps<{
     transition: all .1s;
 }
 
-article:hover > .grow {
-    transform: scale(1.15) rotate(3deg);
+div:hover > .grow {
+    transform: scale(1.15);
 }
 </style>
