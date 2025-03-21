c5318bd897358a900c3aa5fc2e67189bab370421
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-03-21 12:07:05 +0100

    reszponzív viselkedés

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -39,29 +39,29 @@ const meals = computed(() => {
                         <ul class="navbar-nav d-flex flex-row justify-content-center gap-2">
                             <li class="nav-item p-0 m-0">
                                 <RouterLink :to="{ name: 'browse', query: { 'type': MealType.MENU } }" class="nav-link px-3 py-2 rounded-pill" :class="{ 'active': category === MealType.MENU }">
-                                    <font-awesome-icon :icon="['fas', 'utensils']" class="me-2 fs-6 align-baseline" />Menük
+                                    <font-awesome-icon :icon="['fas', 'utensils']" class="me-md-2 fs-6 align-baseline" /><span class="d-none d-md-inline-block">Menük</span>
                                 </RouterLink>
                             </li>
                             <li class="nav-item">
                                 <RouterLink :to="{ name: 'browse', query: { 'type': MealType.FOOD } }" class="nav-link px-3 py-2 rounded-pill" :class="{ 'active': category === MealType.FOOD }">
-                                    <font-awesome-icon :icon="['fas', 'pizza-slice']" class="me-2 fs-6 align-baseline" />Ételek
+                                    <font-awesome-icon :icon="['fas', 'pizza-slice']" class="me-md-2 fs-6 align-baseline" /><span class="d-none d-md-inline-block">Ételek</span>
                                 </RouterLink>
                             </li>
                             <li class="nav-item">
                                 <RouterLink :to="{ name: 'browse', query: { 'type': MealType.BEVERAGE } }" class="nav-link px-3 py-2 rounded-pill" :class="{ 'active': category === MealType.BEVERAGE }">
-                                    <font-awesome-icon :icon="['fas', 'martini-glass-citrus']" class="me-2 fs-6 align-baseline" />Italok
+                                    <font-awesome-icon :icon="['fas', 'martini-glass-citrus']" class="me-md-2 fs-6 align-baseline" /><span class="d-none d-md-inline-block">Italok</span>
                                 </RouterLink>
                             </li>
                             <li class="nav-item">
                                 <RouterLink :to="{ name: 'browse', query: { 'type': MealType.DESSERT } }" class="nav-link px-3 py-2 rounded-pill" :class="{ 'active': category === MealType.DESSERT }">
-                                    <font-awesome-icon :icon="['fas', 'ice-cream']"  class="me-2 fs-6 align-baseline" />Desszertek
+                                    <font-awesome-icon :icon="['fas', 'ice-cream']"  class="me-md-2 fs-6 align-baseline" /><span class="d-none d-md-inline-block">Desszertek</span>
                                 </RouterLink>
                             </li>
                         </ul>
                     </div>
                 </div>
             </header>
-            <div class="row g-3 row-cols-2 row-cols-md-3 row-cols-lg-4">
+            <div class="row g-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
                 <template v-if="meals[category].length === 0">
                     <div class="col" v-for="i in 4" :key="i">
                         <MealCardSkeletonized />
